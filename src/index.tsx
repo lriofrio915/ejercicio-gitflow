23e22ea23e375cc701b3815dcf6e72e03c5c938d
luisRiofrio <lriofrio915@gmail.com>
2023-11-11 10:30:55 -0500

    se crea funcion sumar

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,10 @@ import * as React from 'react';
 export const Thing = () => {
   return <div>the snozzberries taste like snozzberries</div>;
 };
+
+const sumar = (num1: any, num2: any) => {
+  return num1 + num2;
+};
+
+let res = sumar(5, 11);
+console.log(res);
